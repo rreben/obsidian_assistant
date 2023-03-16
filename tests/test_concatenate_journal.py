2c3ae3da2c42301c1c4b648f9e555e2dcad0e704
rreben <rupert.rebentisch@gmail.com>
2023-03-16 14:40:04 +0100

    handling of sample files in tests

--- tests/test_concatenate_journal.py
+++ tests/test_concatenate_journal.py
@@ -1,3 +1,7 @@
+# test_concatenate_journal.py
+# Copyright (c) 2023 Dr. Rupert Rebentisch
+# Licensed under the MIT license
+
 import os
 import datetime
 import shutil
@@ -20,6 +24,10 @@ def test_input_folder(tmpdir):
     shutil.rmtree(str(input_folder))
 
 
+@pytest.fixture
+def example_file_path():
+    return os.path.join(os.path.dirname(__file__), '2023-03-13.md')
+
 def test_process_file(test_input_folder):
     input_folder = test_input_folder
 
@@ -34,6 +42,25 @@ def test_process_file(test_input_folder):
         content = f.read()
         assert 'Text for' in content
         assert '2023-03-16' in content
+
+    # Lösche die Ausgabedatei
+    os.remove('output.md')
+
+
+def test_process_file_next(example_file_path):
+    input_folder = test_input_folder
+
+    # Rufe das Hauptprogramm auf, um die Ausgabedatei zu erstellen
+    process_file(example_file_path, 'output.md')
+
+    # Prüfe, ob die Ausgabedatei erstellt wurde
+    assert os.path.exists('output.md')
+
+    # Prüfe, ob der Text in der Ausgabedatei korrekt ist
+    with open('output.md', 'r') as f:
+        content = f.read()
+        assert 'HH diverse' in content
+        assert 'Something happened today' in content
 
     # Lösche die Ausgabedatei
     os.remove('output.md')
