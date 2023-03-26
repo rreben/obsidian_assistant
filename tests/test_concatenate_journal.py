3a28209b6ff4cf7e218d9ca103e2ca8eba4cd1a6
rreben <rupert.rebentisch@gmail.com>
2023-03-26 22:25:13 +0200

    flake 8 compliance

--- tests/test_concatenate_journal.py
+++ tests/test_concatenate_journal.py
@@ -3,10 +3,8 @@
 # Licensed under the MIT license
 
 import os
-import datetime
-import shutil
 import pytest
-from obsidian_assitant import *
+from obsidian_assitant import process_file
 
 
 @pytest.fixture
@@ -23,10 +21,10 @@ def test_process_complete_file(test_files_path):
     with open('output.md', 'r') as f:
         content = f.read()
         assert '2023-03-13' in content
-        assert not 'HH diverse' in content
+        assert 'HH diverse' not in content
         assert 'Something happened today' in content
-        assert not 'What have you learned today' in content
-        assert not 'Ich habe etwas gelernt' in content
+        assert 'What have you learned today' not in content
+        assert 'Ich habe etwas gelernt' not in content
     os.remove('output.md')
 
 
@@ -36,7 +34,7 @@ def test_process_complete_file_no_regex(test_files_path):
     # Example Datei 2023-03-13.md enthält alle Abschnitte
     process_file(os.path.join(test_folder, '2023-03-13.md'),
                  'output.md',
-                 start_pattern='^\*\*',
+                 start_pattern='^\*\*',  # noqa W605
                  stop_pattern='^xxx')
     assert os.path.exists('output.md')
     with open('output.md', 'r') as f:
@@ -54,14 +52,14 @@ def test_process_complete_file_learnings(test_files_path):
     # Example Datei 2023-03-13.md enthält alle Abschnitte
     process_file(os.path.join(test_folder, '2023-03-13.md'),
                  'output.md',
-                 start_pattern='^\*\*What have you learned',
-                 stop_pattern='^\*\*')
+                 start_pattern='^\*\*What have you learned',  # noqa W605
+                 stop_pattern='^\*\*')  # noqa W605
     assert os.path.exists('output.md')
     with open('output.md', 'r') as f:
         content = f.read()
         assert '2023-03-13' in content
-        assert not 'HH diverse' in content
-        assert not 'Something happened today' in content
+        assert 'HH diverse' not in content
+        assert 'Something happened today' not in content
         assert 'Ich habe etwas gelernt' in content
     os.remove('output.md')
 
@@ -75,9 +73,9 @@ def test_process_last_paragraphs_missing_file(test_files_path):
     with open('output.md', 'r') as f:
         content = f.read()
         assert '2023-03-14' in content
-        assert not 'HH diverse' in content
+        assert 'HH diverse' not in content
         assert 'Something happened today' in content
-        assert not 'Ich habe etwas gelernt' in content
+        assert 'Ich habe etwas gelernt' not in content
     os.remove('output.md')
 
 
@@ -90,9 +88,9 @@ def test_process_missing_paragrpaph_file(test_files_path):
     with open('output.md', 'r') as f:
         content = f.read()
         assert '2023-03-12' in content
-        assert not 'HH diverse' in content
-        assert not 'Something happened today' in content
-        assert not 'Ich habe etwas gelernt' in content
+        assert 'HH diverse' not in content
+        assert 'Something happened today' not in content
+        assert 'Ich habe etwas gelernt' not in content
     os.remove('output.md')
 
 
@@ -105,9 +103,9 @@ def test_process_appended_image(test_files_path):
     with open('output.md', 'r') as f:
         content = f.read()
         assert '2023-03-15' in content
-        assert not 'HH diverse' in content
-        assert not 'Something happened today' in content
-        assert not 'Ich habe etwas gelernt' in content
+        assert 'HH diverse' not in content
+        assert 'Something happened today' not in content
+        assert 'Ich habe etwas gelernt' not in content
         assert '![[8EF7446A-7462-4043-BD68-1C997F24682F.jpeg]]' in content
     os.remove('output.md')
 
@@ -120,10 +118,10 @@ def test_process_just_image(test_files_path):
     assert os.path.exists('output.md')
     with open('output.md', 'r') as f:
         content = f.read()
-        assert not '2023-03-16' in content
-        assert not 'HH diverse' in content
-        assert not 'Something happened today' in content
-        assert not 'Ich habe etwas gelernt' in content
+        assert '2023-03-16' not in content
+        assert 'HH diverse' not in content
+        assert 'Something happened today' not in content
+        assert 'Ich habe etwas gelernt' not in content
         assert '![[dummy.png]]' in content
     os.remove('output.md')
 
@@ -132,14 +130,14 @@ def test_process_learnigs(test_files_path):
     test_folder = test_files_path
 
     # Example Datei 2023-03-16.md nur einen Link auf ein Bild
-    process_file(os.path.join(test_folder, '2023-03-12.md'), 
-                 'output.md', 
-                 start_pattern='^\*\*What have you learned', 
-                 stop_pattern='^\*\*')
+    process_file(os.path.join(test_folder, '2023-03-12.md'),
+                 'output.md',
+                 start_pattern='^\*\*What have you learned',  # noqa W605
+                 stop_pattern='^\*\*')  # noqa W605
     assert os.path.exists('output.md')
     with open('output.md', 'r') as f:
         content = f.read()
         assert '2023-03-12' in content
         assert 'Ich habe etwas gelernt' in content
-        assert not '![[dummy.png]]' in content
+        assert '![[dummy.png]]' not in content
     os.remove('output.md')
